c9dfdabbef1a15e4f8ec6bd5e8dcb6e1f491da1f
shadrack959 <shadikips959@gmail.com>
2023-02-19 10:08:39 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -prints all possible different combinations of three digits.
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int  i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
