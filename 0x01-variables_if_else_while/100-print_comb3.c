a35b0ebffb70f94b168f6520600831134f8cfca8
shadrack959 <shadikips959@gmail.com>
2023-02-19 10:24:17 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,11 +8,22 @@
 
 int main(void)
 {
-	int  i;
+	int i;
+	int j;
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		putchar(i);
+		for (j = 1 ; j < 10 ; j++)
+		{
+			if (i < j && i != j)
+
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
 	putchar('\n');
 	return (0);
