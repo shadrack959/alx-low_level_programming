d4bf15892478b6baab0eca8abd24d5c15876a73f
shadrack959 <shadikips959@gmail.com>
2023-02-19 04:04:32 -0800

    3-print_alphabets,c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	for (i = 'A' ; i <= 'Z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
