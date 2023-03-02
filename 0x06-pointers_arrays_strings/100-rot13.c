dddd052f8531d11227e5a05d70fc8aa0141fc6bd
shadrack959 <shadikips959@gmail.com>
2023-03-01 19:49:52 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,21 +8,21 @@
  * Return: *s
  */
 
-char *rot13(char *)
+char *rot13(char *s)
 {
 	int i;
 	int j;
-	char data1[]* =
+	char data1[] =
 "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	dataot[] =
+	char dataot[] =
 "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	for (i = 0; s[i] i = '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
 		{
 			if (s[i] == data1[j])
 			{
-				s[i] = datarot[j];
+				s[i] = dataot[j];
 				break;
 			}
 		}
