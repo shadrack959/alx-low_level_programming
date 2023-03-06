960c6e06f2b084540893dd71d078bad702cd6272
shadrack959 <shadikips959@gmail.com>
2023-03-06 10:26:30 -0800

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,7 +13,7 @@ unsigned int _strspn(char *s, char *accept)
 
 	while (s[i] != '\0') /*iterate through string*/
 	{
-		for (j = 0; accept[j] != '\0'; j++) /*iterate through target*/
+	for (j = 0; accept[j] != '\0'; j++) /*iterate through target*/
 	{
 	if (s[i] == accept[j]) /*record & break at first match*/
 	{
