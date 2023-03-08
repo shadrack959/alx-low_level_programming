db69723da72e06c1a063f31e6ab0cfb5aaa813b9
shadrack959 <shadikips959@gmail.com>
2023-03-08 05:45:02 -0800

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,12 +1,15 @@
 #include "main.h"
 
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+int is_palindrome(char *s);
+
 /**
  * is_palindrome - returns 1 if a string is a palindrome and 0 if not
  * @s: The string to be measured
  *
  * Return: The length of the string
  */
-
 int is_palindrome(char *s)
 {
 	int len = 0;
