54bda3e3fbe33d5363bb6a863edcd8ed9a0e917f
shadrack959 <shadikips959@gmail.com>
2023-03-23 03:53:13 -0700

    header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
-#ifndef VARIADIC _H_
-#define VARIADIC _H_
+#ifndef VARIADIC_H_
+#define VARIADIC__H_
+
 int _putchar (char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
