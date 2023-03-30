f0b0dcb80d223b00ae062010e3434664525ada55
shadrack959 <shadikips959@gmail.com>
2023-03-30 11:05:09 -0700

    header file

--- lists.h
+++ lists.h
@@ -1,29 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stddef.h>
-/**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
- */
-typedef struct listint_s
-{
-	int n;
-	struct listint_s *next;
-} listint_t;
-/**
- * struct listadd_s - singly linked list
- * @address: Address of a node in a list
- * @next: Pointer to next node
- */
-typedef struct listadd_s
-{
-	void *address;
-	struct listadd_s *next;
-} listadd_t;
+
+int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
